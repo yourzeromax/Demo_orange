d5ea38c99aa16bc9443e28047267e954a966d521
yourzeromax <yourzeromax@163.com>
2018-06-02 18:02:41 +0800

    Initial commit

---
+++ pages/welcome/welcome.js
@@ -0,0 +1,82 @@
+// pages/welcome/welcome.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userName: "yourzeromax"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+console.log("welcome page is unload!")
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  onTapJump: function () {
+    wx.navigateTo({
+      url: '../post/post',
+      fail: function () {
+
+      },
+      success: function () {
+
+      },
+      complete: function () {
+
+      }
+    });
+
+  }
+})
